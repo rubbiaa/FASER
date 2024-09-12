754be38c591cfe0276c26a8a4ae7a9042c98cd7b
Andre Rubbia <andre.rubbia@cern.ch>
2024-09-12 03:42:15 +0200

    add Analysis folder

---
+++ Analysis/AnalyReco.cc
@@ -0,0 +1,177 @@
+// Analysis of reconstructed events of FASERCAL events
+//
+// A. Rubbia, September 2024
+//
+
+#include <string>
+
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include <TGeoManager.h>
+
+#include "TcalEvent.hh"
+#include "TPORecoEvent.hh"
+#include "TTauSearch.hh"
+#include "TParticleGun.hh"
+
+struct EVENT {
+    Int_t t_reaction; // reaction 1=nue, 2=numu, 3=nutau, +10 if NC, 20=ES (truth)
+    Int_t n_clusters;
+    Float_t c_E1;    // energy most energetic cluster
+    Float_t c_E2;    // energy 2nd most energetic cluster
+};
+
+int main(int argc, char** argv) {
+
+    struct EVENT event;
+
+	if (argc < 2) {
+	std::cout << "Usage: " << argv[0] << " <run> [maxevent] [mask]" << std::endl;
+        std::cout << "   <run>                     Run number" << std::endl;
+        std::cout << "   maxevent                  Maximum number of events to process (def=-1)" << std::endl;
+        std::cout << "   mask                      To process only specific events (def=none): ";
+        std::cout << "  nueCC, numuCC, nutauCC, nuNC or nuES" << std::endl;
+		return 1;
+	}
+
+   // get the run number as the first argument
+    std::string runString = argv[1];
+    int run_number;
+
+    try {
+        run_number = std::stoi(runString);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Invalid argument for run: " << e.what() << std::endl;
+        exit(1);
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Out of range for run: " << e.what() << std::endl;
+        exit(1);
+    }
+
+    int max_event = -1;
+    if(argc>2) {
+        try {
+            max_event = std::stoi(argv[2]);
+        } catch (const std::invalid_argument& e) {
+            std::cerr << "Invalid argument for maxevent: " << e.what() << std::endl;
+            exit(1);
+        } catch (const std::out_of_range& e) {
+            std::cerr << "Out of range for maxevent: " << e.what() << std::endl;
+            exit(1);
+        }
+    }
+
+    int event_mask = 0;
+    if(argc>3) {
+        int mask = TPOEvent::EncodeEventMask(argv[3]);
+        if(mask>0) {
+            event_mask = mask;
+        } else {
+            std::cerr << "Unknown mask " << argv[3] << std::endl;
+            exit(1);            
+        }
+    }
+
+    std::string base_path = "input/";
+
+    std::ostringstream filename;
+    filename << "Analysis_" << run_number;
+    if(event_mask>0) {
+        const char *mask = TPOEvent::DecodeEventMask(event_mask);
+        filename << "_" << mask;
+    }
+    filename << ".root";
+
+    // Print the filename to verify
+    std::cout << "Writing TPORecEvent and histograms into file: " << filename.str() << " ..... " << std::endl;
+
+    TFile *m_rootFile = new TFile(filename.str().c_str(), "RECREATE", "", 0); // last is the compression level
+    if (!m_rootFile || !m_rootFile->IsOpen())
+    {
+        throw std::runtime_error("Could not create ROOT file");
+    }
+    m_rootFile->cd();
+    TTree *t = new TTree("Event", "Event");
+    t->Branch("t_reaction",&event.t_reaction);
+    t->Branch("n_clusters",&event.n_clusters);
+    t->Branch("c_E1", &event.c_E1);
+    t->Branch("c_E2", &event.c_E2);
+
+    std::ostringstream inputfilename;
+    inputfilename << "input/Batch-TPORecevent_" << run_number << ".root";
+
+    TFile *m_inrootFile = new TFile(inputfilename.str().c_str(), "READ"); 
+    if (!m_inrootFile || !m_inrootFile->IsOpen())
+    {
+        return 1;
+    }
+    m_inrootFile->cd();
+    TTree *event_tree;
+    m_inrootFile->GetObject("RecoEvent",event_tree);
+
+    Long_t nentries = event_tree->GetEntries();
+    std::cout << "Number of entries " << nentries << std::endl;
+
+    // TPORecoEvent class and histograms
+    TPORecoEvent *fTPORecoEvent = nullptr; // &fTPORecoEvent;
+    event_tree -> SetBranchAddress("TPORecoEvent", &fTPORecoEvent);
+
+    // process events
+    int ievent = 0;
+    if(max_event == -1) max_event = nentries;
+    int error = 0;
+
+    while (error == 0 && ievent<max_event) {
+
+        event_tree->GetEntry(ievent);
+
+        if(ievent % 1000 == 0) {
+            std::cout << "Processing event " << ievent << std::endl;
+        }
+        bool dump_event_cout = (ievent < 20);
+        if(dump_event_cout) {
+            fTPORecoEvent -> GetPOEvent()->dump_event();
+        }
+        if(fTPORecoEvent -> GetPOEvent()->isES()) {
+            event.t_reaction = 20;
+        } else {
+            event.t_reaction = fTPORecoEvent -> GetPOEvent()->isCC ? 0 : 10;
+            int m_pdg = abs(fTPORecoEvent -> GetPOEvent()->in_neutrino.m_pdg_id);
+            switch(m_pdg){
+                case 12:
+                    event.t_reaction += 1;
+                    break;
+                case 14:
+                    event.t_reaction += 2;
+                    break;
+                case 16:
+                    event.t_reaction += 3;
+                    break;
+                default:
+                break;
+            }
+        }
+        event.n_clusters = fTPORecoEvent->n_psclustersX();
+        if(event.n_clusters>0) {
+            event.c_E1 = fTPORecoEvent->PSClustersX[0].rawenergy/1e3;   // convert to GeV
+        } else {
+            event.c_E1 = 0;
+        }
+        if(event.n_clusters>1) {
+            event.c_E2 = fTPORecoEvent->PSClustersX[1].rawenergy/1e3;   // convert to GeV
+        } else {
+            event.c_E2 = 0;
+        }
+
+        t->Fill();
+        ievent++;
+    }
+
+    m_rootFile->Write();
+    m_rootFile->Close();
+
+    std::cout << "I'm done." << std::endl;
+
+    return 0;
+}
---
+++ Analysis/LinkDef.h
@@ -0,0 +1,39 @@
+#include <Rtypes.h>
+
+#include "TPORecoEvent.hh"
+#include "TTauSearch.hh"
+#include "TParticleGun.hh"
+#include "TPSCluster.hh"
+#include "TTKTrack.hh"
+
+#ifdef __CLING__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class TPORecoEvent+;
+#pragma link C++ class TPORec+;
+#pragma link C++ struct TPORec::CALENERGIES+; 
+#pragma link C++ function TPORec::TotalEvis+;
+#pragma link C++ function TPORec::TotalET+;
+
+#pragma link C++ class TPOEvent+;
+#pragma link C++ class TcalEvent+;
+#pragma link C++ class DigitizedTrack+;
+#pragma link C++ struct PO+;
+
+#pragma link C++ class TTauSearch;
+#pragma link C++ struct TTauSearch::KINEMATICS;
+
+#pragma link C++ class TParticleGun;
+#pragma link C++ struct TParticleGun::FEATURES;
+
+#pragma link C++ class TPSCluster;
+#pragma link C++ struct TPSCluster::PSCLUSTERLONGPROFILE;
+
+#pragma link C++ class TTKTrack;
+
+#endif
