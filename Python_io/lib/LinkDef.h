b2fedd7979520dfb522215881f0cb127e250b877
Till Dieminger | dlnu <tilld@phys.ethz.ch>
2024-09-19 15:37:02 +0200

    Update lib.so fir IO

--- Python_io/lib/LinkDef.h
+++ Python_io/lib/LinkDef.h
@@ -1,5 +1,32 @@
+#include <vector>
+
 // LinkDef.h
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
 #pragma link C++ class DigitizedTrack+;
 #pragma link C++ class std::vector<DigitizedTrack*>+;
 #pragma link C++ class TcalEvent+;
-#pragma link C++ class TcalEvent::GEOM_DETECTOR+;
+#pragma link C++ struct TcalEvent::GEOM_DETECTOR+;
+#pragma link C++ struct TcalEvent::REARCALDEPOSIT+;
+#pragma link C++ class TParticleGun+;
+#pragma link C++ struct TParticleGun::FEATURES+;
+#pragma link C++ class TPOEvent+;
+#pragma link C++ struct PO+;
+#pragma link C++ class TPORec+;
+#pragma link C++ class TPORecoEvent+;
+#pragma link C++ struct TPORec::TRACKHIT+;
+#pragma link C++ struct TPORec::TRACK+;
+#pragma link C++ struct TPORec::CALENERGIES+;
+#pragma link C++ struct TPORec::CALENERGIES+;
+#pragma link C++ struct TPORecoEvent::PSHIT2D+;
+#pragma link C++ struct TPORecoEvent::REARCALS+;
+#pragma link C++ struct TPORecoEvent::PSVOXEL3D+;
+#pragma link C++ class TPSCluster+;
+#pragma link C++ class TPSCluster::PSCLUSTERHIT+;
+#pragma link C++ class TPSCluster::PSCLUSTERLONGPROFILE+;
+#pragma link C++ class TPSTrack+;
+#pragma link C++ class TPSTrack::TRACKHIT+;
+#pragma link C++ class TTauSearch+;
+#pragma link C++ class TTauSearch::KINEMATICS+;
+#pragma link C++ class TTKTrack+;
+#pragma link C++ class TTKTrack::TRACKHIT+;
