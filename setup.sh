12190ea37a55215e0258cf46ae33f5afdef8bed4
André Rubbia <rubbiaa@phys.ethz.ch>
2024-12-03 14:45:30 +0100

    extend setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
+HOME=/home/rubbiaa/FASER
+
 source /home/rubbiaa/ROOT/root_install_v6.32.02/bin/thisroot.sh
 source /home/rubbiaa/geant4-install/bin/geant4.sh
 export PYTHIA8=/home/rubbiaa/ROOT/pythia8312
 
+export CLHEPINSTALL=$HOME/CLHEP-install
+export RAVEINSTALL=$HOME/rave-install
+export GENFITINSTALL=$HOME/GenFit-install
+export LD_LIBRARY_PATH=$GENFITINSTALL/lib:$RAVEINSTALL/lib:$CLHEPINSTALL/lib:$LD_LIBRARY_PATH
