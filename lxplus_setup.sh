efec475ea4ca0a3b0f8d6984edb043c61f89b53d
Andre Rubbia <andre.rubbia@cern.ch>
2024-12-10 19:06:53 +0100

    fix setup for lxplus

--- lxplus_setup.sh
+++ lxplus_setup.sh
@@ -1,11 +1,11 @@
 export HOMEFASER=/eos/home-r/rubbiaa/FASER
 
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.34.00/x86_64-almalinux9.5-gcc115-opt/bin/thisroot.sh
+source $HOMEFASER/root-install/bin/thisroot.sh
 pushd .
 cd /cvmfs/geant4.cern.ch/geant4/11.2.p01/x86_64-el9-gcc11-optdeb/bin/; source geant4.sh
 popd
 
-# export PYTHIA8=/home/rubbiaa/ROOT/pythia8312
+export PYTHIA8=$HOMEFASER/pythia8312
 
 export CLHEPINSTALL=$HOMEFASER/CLHEP-install
 export RAVEINSTALL=$HOMEFASER/rave-install
